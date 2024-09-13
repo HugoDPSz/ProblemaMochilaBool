e0b8bc110f186bf34b3b1e2da452d2d0616169f9
HugoDPSz <128089628+HugoDPSz@users.noreply.github.com>
2024-09-13 17:11:50 -0300

    Add files via upload

--- funcionalidades.h
+++ funcionalidades.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct elemento{
     int valor;
--- maxRelacao.c
+++ maxRelacao.c
@@ -1,19 +1,27 @@
 #include "funcionalidades.h"
 
 void resolucaoMochila(elemento* elementos, int qtd, int pesoMax){
-    int soma=0, i=0;
+    int soma=0, i=0, valor=0;
 
-    //Em construção
     while(soma<pesoMax && i<qtd){
         if(soma + elementos[i].peso < pesoMax){
-            soma+= elementos[i].peso;
+            soma += elementos[i].peso;
+            valor += elementos[i].valor;
             elementos[i].entrou = 1;
         }
         i++;
     }
+    printf("Valor total obtido: %d\n", valor);
 }
 
 int main(){
+    clock_t start, end;
+    double cpu_time_used;
+
+    start = clock();
+    
+    printf("Iniciando o programa...\n");
+
     FILE *arq = fopen("knapPI_1_100_1000_1", "r");
     int qtd, pesoMax;
     infoMochila(arq, &qtd, &pesoMax);
@@ -29,6 +37,11 @@ int main(){
     output(elementos, qtd);
 
     free(elementos);
+
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    
+    printf("Tempo de execução: %f segundos\n", cpu_time_used);
 
     return 0;
 }
